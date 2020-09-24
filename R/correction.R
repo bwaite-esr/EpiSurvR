5e144c420377e65ef9e486718e4f02041ce958d4
Giles Graham <Giles.Graham@esr.cri.nz>
2020-09-24 14:48:21 +1200

    Added a function for data cleaning.

---
+++ R/correction.R
@@ -0,0 +1,19 @@
+#' Correction to the dataset
+#'
+#' a convenience function, to be used within spot_clean
+#' passes an individual correction
+#'
+#' @param given_cond a boolean expression. the conditions to meet e.g. epiSurvNumber == "##-######-XX" etc
+#' @param given_edit a named vector of corrections e.g. c(age = "2 yrs",sex = "Male")
+#' @keywords EpiSurv
+#' @keywords EpiSurv Numbers
+#' @export
+#' @examples
+#' # no examples
+#' 
+correction <- function(given_cond,given_edit){
+  list(
+    condition = expression(given_cond),
+    change = expresson(given_edit)
+  )
+}
---
+++ R/spot_clean.R
@@ -0,0 +1,32 @@
+#' Spot clean a dataset
+#'
+#' takes a list of corrections
+#' and applies them to the dataset
+#'
+#' @param data your extract
+#' @param adjustments a list of corrections. see ?correction
+#' @keywords EpiSurv
+#' @keywords data cleaning
+#' @export
+#' @examples
+#' # no examples
+#' @import dplyr
+#' @import magrittr
+#' 
+
+spot_clean <- function(data,adjustments){
+  data <- data %>% mutate(index = row_number())
+  for(i in adjustments){
+    
+    temp <- data %>% filter(eval(i$condition)) 
+    index <- temp$index
+    adj <- temp %>% mutate(
+      !!!eval(i$change)
+    )
+    
+    data[index,] <- adj
+    
+  }
+  return(data)
+}
+
